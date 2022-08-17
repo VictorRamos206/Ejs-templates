a32f70e9d2667cf0113e627e4220776318130cf1
VRamosw206 <103076868+VictorRamos206@users.noreply.github.com>
2022-08-17 06:13:04 +0000

    Done

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine','ejs')
+
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+  var title = "Home"
+  var heading = "Victors Favorite Things"
+  res.render('pages/index',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/Soccer', (req, res) => {
+  var title = "Victors Favorite soccer  team is  FC Barcelona"
+  var heading = 'My Website'
+  res.render('pages/soccer',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/Games', (req, res) => {
+  var title = "Best Game of 2022 is Warzone in my opinion"
+  var heading = 'My Website'
+  res.render('pages/games',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/Players', (req, res) => {
+  var title = "Best Soccer player ever is Lionel Messi"
+  var heading = 'My Website'
+  res.render('pages/players',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
